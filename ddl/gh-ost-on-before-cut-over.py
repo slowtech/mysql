3284ab5834517392c7fad3d1e492d464d161bc92
slowtech <ivictor@foxmail.com>
2021-12-30 08:32:20 +0800

    Update gh-ost-on-before-cut-over.py

--- ddl/gh-ost-on-before-cut-over.py
+++ ddl/gh-ost-on-before-cut-over.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import os,yagmail
 GH_OST_MIGRATED_HOST=os.environ.get('GH_OST_MIGRATED_HOST')
@@ -6,7 +6,7 @@ GH_OST_DATABASE_NAME=os.environ.get('GH_OST_DATABASE_NAME')
 GH_OST_TABLE_NAME=os.environ.get('GH_OST_TABLE_NAME')
 GH_OST_DDL=os.environ.get('GH_OST_DDL')
 
-message_head="Before cut-over:"+"h="+GH_OST_MIGRATED_HOST+",D="+GH_OST_DATABASE_NAME+",t="+GH_OST_TABLE_NAME+"DDL: "+GH_OST_DDL
+message_head="Before cut-over: h=%s, D=%s, t=%s, DDL: %s"%(GH_OST_MIGRATED_HOST, GH_OST_DATABASE_NAME, GH_OST_TABLE_NAME, GH_OST_DDL)
 yag = yagmail.SMTP(user='slowtech@126.com',password='******',host='smtp.126.com')
 contents = [message_head]
-yag.send('ivictor@foxmail.com', message_head, contents)
+yag.send('slowtech@126.com', message_head, contents)
